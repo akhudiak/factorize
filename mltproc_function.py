4634c1b414f37c24672d873bba090d04cefb460f
akhudiak <artem.khudiak@gmail.com>
2023-11-20 10:02:40 +0200

    Time function
    Function time in synchronous version: 42.33 s.

--- mltproc_function.py
+++ mltproc_function.py
@@ -1,3 +1,6 @@
+from time import time
+
+
 def factorize(*number) -> list[list[int]]:
 
     function_result = []
@@ -9,9 +12,14 @@ def factorize(*number) -> list[list[int]]:
     return function_result
 
 
-a, b, c, d  = factorize(128, 255, 99999, 10651060)
+def func_time(start_time):
+    return time() - start_time
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
+if __name__ == "__main__":
+
+    start_time = time()
+    
+    factorize(123456789, 84248932, 705784658, 10651060)
+
+    print(f"Function time: {func_time(start_time)}")
